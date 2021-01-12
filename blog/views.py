0cfc6c1ba5aba8bbb137e54e5ac747223477f8e5
cejimenez0123 <christianjimenez0123@gmail.com>
2021-01-12 15:36:48 -0500

    views

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,9 @@ from .models import Post
 
 def post_list(request): 
     posts = Post.published.all() 
-    return render(request, 
-        'blog/post/list.html', 
-        {'posts': posts}) 
+    context={'posts': posts}
+    return render(request,'post/list.html', 
+        context) 
 
 def post_detail(request, year, month, day, post): 
     post = get_object_or_404(Post, slug=post, 
@@ -13,6 +13,6 @@ def post_detail(request, year, month, day, post):
     publish__year=year, 
     publish__month=month,  publish__day=day) 
     return render(request, 
-    'blog/post/detail.html', 
+    'post/detail.html', 
     {'post': post}) 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 
 from django.urls import path, include 
 from django.contrib import admin 
+from blog import views
 urlpatterns = [ 
- path('admin/', admin.site.urls), 
- path('blog/', include('blog.urls', namespace='blog')), ] 
+    path('',views.post_list,name="post_list"),
+    path('admin/', admin.site.urls), 
+    path('blog/', include('blog.urls', namespace='blog')), ] 
