7e7213de831ea66c4f3197308aab6f3e674f67fd
cejimenez0123 <christianjimenez0123@gmail.com>
2021-01-15 13:43:00 -0500

    hope

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import smtplib
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,6 +21,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ue0f9s^q8#nids&&y9g676bv_ikpb8j9276d=*=2p0b-00-zak'
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    # ...send emails
+except:
+    print ('Something went wrong...')
+
+    
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,10 @@ from blog import views
 
 
 urlpatterns = [
-    path('',views.post_list,name="post_list"),
+    path('', views.PostListView.as_view(), name='post_list'),
+    # path('',views.post_list,name="post_list"),
+    path('blog/<int:year>/<int:month>/<int:day>/<slug:post>',views.post_detail,name='post_detail'),
+    path('<int:post_id>/share/',views.post_share, name='post_share'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
     path('test/', views.test, name='test')
